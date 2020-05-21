6a57a2d268e10f81b737a6db494b3b6e32c95e99
Matheus Dias <matheusmelhor3@outlook.com>
2020-05-21 01:40:31 -0300

    com interface

--- src/model/services/BrasilTaxService.java
+++ src/model/services/BrasilTaxService.java
@@ -1,8 +1,8 @@
 package model.services;
 
-public class BrasilTaxService {
+public class BrasilTaxService  implements TaxService{
 	
-	public double tax(double amount) {
+	public double tax(double amount){
 		if(amount <= 100.0) {
 			return amount * 0.2;
 		}
